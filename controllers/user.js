d98863e98a8f0497af9f078007615c58a247742b
Charles <charles081889@gmail.com>
2019-11-06 15:56:27 +0100

    tried fixing the check-if-user-exists feature in user controller

--- controllers/user.js
+++ controllers/user.js
@@ -15,27 +15,29 @@ exports.createUser = (req, res) => {
       if (boolValue.rows[0].bool === true) {
         return res.status(400).json({ error: 'An exact email already exist in the database' });
       }
-      bcrypt.hash(password, 10)
-        .then((hash) => {
-          password = hash;
-          query(`INSERT INTO users (
+      if (boolValue.rows.length === 0 && boolValue.rows[0].bool === false) {
+        bcrypt.hash(password, 10)
+          .then((hash) => {
+            password = hash;
+            query(`INSERT INTO users (
           firstName, lastName, email, password, gender, jobRole, department, address, account_type
           ) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9) RETURNING *`, [
-            firstName, lastName, email, password,
-            gender, jobRole, department, address, accountType])
-            .then((result) => res.status(201).json({
-              status: 'success',
-              data: {
-                message: 'User account successfully created',
-                token: req.headers.authorization.split(' ')[1],
-                userId: result.rows[0].id, // cross check
-              },
-            }))
-            .catch((err) => res.status(400).json({
-              status: 'failure',
-              error: `User account not created, ${err}`,
-            }));
-        });
+              firstName, lastName, email, password,
+              gender, jobRole, department, address, accountType])
+              .then((result) => res.status(201).json({
+                status: 'success',
+                data: {
+                  message: 'User account successfully created',
+                  token: req.headers.authorization.split(' ')[1],
+                  userId: result.rows[0].id, // cross check
+                },
+              }))
+              .catch((err) => res.status(400).json({
+                status: 'failure',
+                error: `User account not created, ${err}`,
+              }));
+          });
+      }
     })
     .catch((err) => res.status(400).json({
       status: 'failure',
