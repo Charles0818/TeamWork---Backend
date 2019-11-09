e0386bfdcb9fcd78ddbfb2b2a2a3ca3b870cfaaf
Charles <charles081889@gmail.com>
2019-11-09 18:27:58 +0100

    modified database config and travis.yml

--- config/db.js
+++ config/db.js
@@ -3,12 +3,7 @@ const { Pool } = require('pg');
 require('dotenv').config();
 
 const isProduction = process.env.NODE_ENV === 'production';
-const isTest = process.env.NODE_DEV === 'test';
-const {
-  DB_USER, DB_PASSWORD, DB_PASS, DB_HOST, DB_PORT, DB_DATABASE,
-} = process.env;
-const connectionString = `postgresql://${DB_USER}:${isTest ? DB_PASS : DB_PASSWORD}@${DB_HOST}:${DB_PORT}/${DB_DATABASE}`;
-
+const connectionString = 'postgresql://postgres:charlieboy@localhost:5432/teamwork';
 
 const pool = new Pool({
   connectionString: isProduction ? process.env.DATABASE_URL : connectionString,
