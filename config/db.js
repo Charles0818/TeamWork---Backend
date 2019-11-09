0da8d49b20e22951264d4a0a7c048ac241d39ac5
Charles <charles081889@gmail.com>
2019-11-09 17:50:24 +0100

    modified travis config & package.json

--- config/db.js
+++ config/db.js
@@ -3,10 +3,11 @@ const { Pool } = require('pg');
 require('dotenv').config();
 
 const isProduction = process.env.NODE_ENV === 'production';
+const isTest = process.env.NODE_DEV === 'test';
 const {
-  DB_USER, DB_PASSWORD, DB_HOST, DB_PORT, DB_DATABASE,
+  DB_USER, DB_PASSWORD, DB_PASS, DB_HOST, DB_PORT, DB_DATABASE,
 } = process.env;
-const connectionString = `postgresql://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/${DB_DATABASE}`;
+const connectionString = `postgresql://${DB_USER}:${isTest ? DB_PASS : DB_PASSWORD}@${DB_HOST}:${DB_PORT}/${DB_DATABASE}`;
 
 
 const pool = new Pool({
