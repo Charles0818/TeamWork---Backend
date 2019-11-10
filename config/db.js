0d476c86dc34c94f9f2c765e962e124d7f385af0
Charles <charles081889@gmail.com>
2019-11-10 16:29:51 +0100

    modified database config

--- config/db.js
+++ config/db.js
@@ -3,13 +3,13 @@ const { Pool } = require('pg');
 require('dotenv').config();
 
 const isProduction = process.env.NODE_ENV === 'production';
-const connectionString = 'postgresql://postgres:charlieboy@localhost:5432/teamwork';
+const connectionString = 'postgres://admin:charlieboy@localhost:5432/teamwork';
 
 const pool = new Pool({
   connectionString: isProduction ? process.env.DATABASE_URL : connectionString,
   ssl: isProduction,
 });
-pool.connect().then(() => console.log('Connected to the database'))
+pool.connect().then(() => console.log('Connected to database'))
   .catch((err) => console.log(`Unable to connect to database, ${err}`));
 
 const query = (text, params) => pool.query(text, params);
