84012a8305f100b730cad118eb1f8d850aeb74fd
Charles <charles081889@gmail.com>
2019-11-11 01:11:30 +0100

    added new dependencies

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,10 @@
 module.exports = {
     "extends": "airbnb-base",
+    "rules": {
+        "no-console": "off",
+        "func-names": "off",
+        "no-process-exit": "off",
+        "class-methods-use-this": "off"
+    }
     
 }
