d2096d3b79c10432535e19feaf1d9d1f27b1f4e9
Charles <charles081889@gmail.com>
2019-11-26 06:22:05 +0100

    fixed cors error

--- app.js
+++ app.js
@@ -1,5 +1,4 @@
 const express = require('express');
-const cors = require('cors');
 // routes;
 const userRoutes = require('./routes/user');
 const gifRoutes = require('./routes/gif');
@@ -9,7 +8,12 @@ const commentRoutes = require('./routes/comments');
 
 const { urlencoded } = express;
 const app = express();
-app.use(cors());
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+  next();
+});
 app.use(express.json());
 app.use(urlencoded({ extended: false }));
 app.use('/api/v1/auth', userRoutes);
