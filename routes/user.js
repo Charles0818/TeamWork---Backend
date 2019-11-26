4401b44f2ae2e36a4db4882405195fe6f255f963
Charles <charles081889@gmail.com>
2019-11-26 17:12:54 +0100

    removed auth from create-user route for first account creation sake

--- routes/user.js
+++ routes/user.js
@@ -5,7 +5,7 @@ const userCtrl = require('../controllers/user');
 
 const router = express.Router();
 
-router.post('/create-user', auth, userCtrl.createUser);
+router.post('/create-user', userCtrl.createUser);
 router.post('/signin', userCtrl.login);
 router.put('/users/:userId', auth, multer.any(), userCtrl.updateUserPic);
 router.delete('/users/:employeeId/:userId', auth, userCtrl.deleteUser);
